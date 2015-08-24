f347191ca37b397b9a7421124b334f44a6b9541d
Marc Ramirez <mrami@bluecirclesoft.com>
2015-08-24 00:05:47 -0400

    Add licenses.

--- src/main/java/com/bluecirclesoft/open/getopt/converters/BigDecimalConverter.java
+++ src/main/java/com/bluecirclesoft/open/getopt/converters/BigDecimalConverter.java
@@ -1,3 +1,21 @@
+/*
+
+Copyright 2015 Blue Circle Software, LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ */
+
 package com.bluecirclesoft.open.getopt.converters;
 
 import com.bluecirclesoft.open.getopt.CommandLineOptionException;
@@ -6,7 +24,7 @@ import com.bluecirclesoft.open.getopt.TypeConverter;
 import java.math.BigDecimal;
 
 /**
- * TODO document me
+ * Converter to parse parameter strings into BigDecimals.
  */
 public class BigDecimalConverter implements TypeConverter<BigDecimal> {
 
